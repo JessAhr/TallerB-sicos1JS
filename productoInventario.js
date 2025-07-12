2c76cd9c9291469886f084e4750972a0ac3fc8de
karen M ulcue L <looshiri1@gmail.com>
2025-07-12 10:32:19 -0500

    cambios en funcion buscar producto

--- productoInventario.js
+++ productoInventario.js
@@ -8,11 +8,6 @@
 //  ];
 const prompt  = require ("prompt-sync")();
 
-function buscarProducto (nombre, inventario){
-    
-
-}
-
 const inventario =  [ 
 {nombre: "rosas", precio: 7000},
 {nombre: "peluche pqueño", percio: 11000},
@@ -20,5 +15,24 @@ const inventario =  [
 {nombre: "ramo x7", precio:50000},
 {nombre: "estuche de chocolates", precio: 23000}
 
-]
+];
 
+function buscarProducto (nombre, inventario){
+    for (let producto of inventario) {
+      if (producto.nombre.toLowerCase() === nombre.toLowerCase()){
+        return "Si existe el producto " ;
+
+        }
+    }
+   return null; 
+
+}
+
+let nombreBuscado = prompt("Ingresa el nombre del producto a buscar: ");
+let resultado = buscarProducto(nombreBuscado, inventario);
+
+if (resultado !== null) {
+  console.log(resultado); 
+} else {
+  console.log(null);
+}
